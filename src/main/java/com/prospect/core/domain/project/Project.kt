121f27a0750848efcc19c61f7ab358b500beafe8
bookstore <booookstore@icloud.com>
2019-04-27 17:50:43 +0900

    fix : Project has featureId

--- src/main/java/com/prospect/core/domain/project/Project.kt
+++ src/main/java/com/prospect/core/domain/project/Project.kt
@@ -4,16 +4,16 @@ import com.prospect.core.domain.feature.Feature
 
 class Project(
         val id: String,
-        private var iceBoxItems: Set<Feature> = setOf(),
+        private var iceBoxItems: Set<String> = setOf(),
         private var productBacklogItems: ProductBacklogItems = ProductBacklogItems()
 ) {
 
-    fun addIceBoxItem(feature: Feature) {
-        iceBoxItems += feature
+    fun addIceBoxItem(featureId: String) {
+        iceBoxItems += featureId
     }
 
-    fun removeIceBoxItem(feature: Feature) {
-        iceBoxItems -= feature
+    fun removeIceBoxItem(featureId: String) {
+        iceBoxItems -= featureId
     }
 
     fun addProductBacklogItem(productBacklogItem: ProductBacklogItem) {
