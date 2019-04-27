d724e05595feee4a1f979697324371914dd5065a
bookstore <booookstore@icloud.com>
2019-04-27 21:17:22 +0900

    refactor : inline List<ProductBacklogItem>

--- src/main/java/com/prospect/core/domain/project/ProductBacklogItems.kt
+++ src/main/java/com/prospect/core/domain/project/ProductBacklogItems.kt
@@ -2,10 +2,10 @@ package com.prospect.core.domain.project
 
 import com.prospect.core.domain.type.Priority
 
-data class ProductBacklogItems(private val items: Items = listOf()) {
+data class ProductBacklogItems(private val items: List<ProductBacklogItem> = listOf()) {
 
     companion object {
-        fun of(lamb: () -> Items): ProductBacklogItems = ProductBacklogItems(lamb())
+        fun of(lamb: () -> List<ProductBacklogItem>): ProductBacklogItems = ProductBacklogItems(lamb())
     }
 
     fun add(productBacklogItem: ProductBacklogItem): ProductBacklogItems = of {
@@ -54,34 +54,32 @@ data class ProductBacklogItems(private val items: Items = listOf()) {
             items.contains(productBacklogItem)
 }
 
-private typealias Items = List<ProductBacklogItem>
-
-private fun Items.divideByPriority(priority: Priority): Pair<Items, Items> {
+private fun List<ProductBacklogItem>.divideByPriority(priority: Priority): Pair<List<ProductBacklogItem>, List<ProductBacklogItem>> {
     val under = filter { it.priority <= priority }
     val over = filter { it.priority > priority }
     return Pair(over, under)
 }
 
-private fun Items.downPriority(): Items = map { it.downPriority() }
+private fun List<ProductBacklogItem>.downPriority(): List<ProductBacklogItem> = map { it.downPriority() }
 
-private fun Items.upPriority(): Items = map { it.upPriority() }
+private fun List<ProductBacklogItem>.upPriority(): List<ProductBacklogItem> = map { it.upPriority() }
 
-private fun Items.add(productBacklogItem: ProductBacklogItem): Items {
+private fun List<ProductBacklogItem>.add(productBacklogItem: ProductBacklogItem): List<ProductBacklogItem> {
     val mutableList = mutableListOf<ProductBacklogItem>()
     mutableList.addAll(this)
     mutableList.add(productBacklogItem)
     return mutableList
 }
 
-private fun Items.remove(productBacklogItem: ProductBacklogItem): Items {
+private fun List<ProductBacklogItem>.remove(productBacklogItem: ProductBacklogItem): List<ProductBacklogItem> {
     val mutableList = mutableListOf<ProductBacklogItem>()
     mutableList.addAll(this)
     mutableList.remove(productBacklogItem)
     return mutableList
 }
 
-private fun Items.findByPriority(priority: Priority): ProductBacklogItem? = firstOrNull { it.priority == priority }
+private fun List<ProductBacklogItem>.findByPriority(priority: Priority): ProductBacklogItem? = firstOrNull { it.priority == priority }
 
-private fun Items.lastPriority(): ProductBacklogItem? = sortedBy { it.priority }.firstOrNull()
+private fun List<ProductBacklogItem>.lastPriority(): ProductBacklogItem? = sortedBy { it.priority }.firstOrNull()
 
-private fun Items.containsByFeatureId(featureId: String): Boolean = any { it.featureId == featureId }
+private fun List<ProductBacklogItem>.containsByFeatureId(featureId: String): Boolean = any { it.featureId == featureId }
