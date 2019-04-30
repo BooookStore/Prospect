503fbf8a73e0a991b8b4c2269ffb20c03653c242
bookstore <booookstore@icloud.com>
2019-04-30 16:15:31 +0900

    feat : project query in controller

--- src/main/java/com/prospect/core/web/ProjectController.kt
+++ src/main/java/com/prospect/core/web/ProjectController.kt
@@ -1,11 +1,22 @@
 package com.prospect.core.web
 
 import com.prospect.core.applicationservice.project.ProjectApplicationService
+import com.prospect.core.query.model.Criteria
+import com.prospect.core.query.model.ProjectOverviewModel
+import com.prospect.core.query.model.ProjectQuery
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("project")
-class ProjectController(private val projectApplicationService: ProjectApplicationService) {
+class ProjectController(
+        private val projectApplicationService: ProjectApplicationService,
+        private val projectQuery: ProjectQuery
+) {
+
+    @GetMapping
+    fun queryOverview(@RequestBody criteria: Criteria): List<ProjectOverviewModel> {
+        return projectQuery.find(criteria)
+    }
 
     @PostMapping
     fun create(@RequestBody aCommand: CreateProjectJSONCommand) {
